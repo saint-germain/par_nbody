da81728a44065afc48a8675b7b03b41e980d27ca
German Chaparro <germanchaparro@gfif.udea.edu.co>
2021-03-03 08:49:42 -0500

    256 run

--- task.sh
+++ task.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#PBS -N million
+#PBS -N million_256
 #PBS -e errores.out
 #PBS -o output.out
 #PBS -M german.chaparro@udea.edu.co
 #PBS -m b
 #PBS -m e
 #PBS -q long
-#PBS -l nodes=4:ppn=12
+#PBS -l nodes=4:ppn=16
 cd $PBS_O_WORKDIR
 python3 parallel_sketch.py no_pert_nb
